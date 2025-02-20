da8c4355104325c55f1b2514a53a3d7fa711ba85
pranay <moolipranayreddy@presidio.com>
2025-02-20 16:16:37 +0530

    bucket changes

--- main.tf
+++ main.tf
@@ -1,27 +1,27 @@
-# terraform {
-#     backend "s3" {
-#     bucket         = "pt-terraform-state-bucket-latest"
-#     key    = "terraform/jenkins/statefile.tfstate"
-#     region = "us-east-1"
-#     dynamodb_table = "terraform-lock-pranay"
+terraform {
+    backend "s3" {
+    bucket         = "my-terraform-state-bucket-latest"
+    key    = "terraform/jenkins/statefile.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-lock"
 
-#   }
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 5.0"
-#     }
-#   }
-#   required_version = "~> 1.5"
-# }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = "~> 1.5"
+}
 
-# provider "aws" {
-#   default_tags {
-#     tags = {
-#       Environment = "training"
-#     }
-#   }
-# }
+provider "aws" {
+  default_tags {
+    tags = {
+      Environment = "training"
+    }
+  }
+}
 
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "pt-tf-bucket-training"
