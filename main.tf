a2555c39908651720dd0de435b35bb9cfaedc452
Pranay Teja M <79045341+Pranayteja15@users.noreply.github.com>
2025-02-21 09:41:58 +0530

    modified bucket

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "pt-tf-bucket-training"
+  bucket = "pt-tf-bucket-training-prod"
   tags = {
     CreadtedBy = "pranay"
   }
